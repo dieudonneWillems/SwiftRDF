8e67d504f83456acf60c558f2be959608775122d
Dieudonné Willems <don.willems@wur.nl>
2016-01-18 18:24:57 +0100

    Added Initial subproject Ontologist

---
+++ Ontologist/Ontologist/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ontologist
+//
+//  Created by Don Willems on 18/01/16.
+//  Copyright © 2016 lapsedpacifist. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
